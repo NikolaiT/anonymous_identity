e8de6701cd2a4ea662d4b02332cb564bef828670
NikolaiT <nikolai.tschacher@hotmail.com>
2013-05-31 23:46:47 +0200

    Compared to PEP8 proposals

--- identity_generator.py
+++ identity_generator.py
@@ -2,6 +2,11 @@ import socks
 import urllib.request
 import re
 import lxml.html
+import time
+
+### Author: Nikolai Tschacher
+### Site: incolumitas.com
+### 30.05.2013
 
 # Read that: http://lxml.de/parsing.html#parsing-html
 
@@ -11,6 +16,8 @@ COUNTRY_CODES = ['as', 'au', 'bg', 'ca', 'cyen', 'cygk', 'dk', 'fi',
 NAMESET_CODES = [ 'ar', 'au', 'ch', 'dk', 'en', 'er', 'fa', 'fi', 'fr',
                  'gd', 'gr', 'hr', 'hu', 'ig', 'is', 'it', 'jp',
                  'jpja', 'nl', 'pl', 'sl', 'sp', 'sw', 'us', 'vn', 'zhtw']
+                 
+FAKENAME_GENERATOR_URL = 'http://fakenamegenerator.com/gen-{0}-{1}-{2}.php'
 
 def test_anonymity(ip='127.0.0.1', port=9050):
     """Test the socks module and verify that the IP address is 
@@ -36,7 +43,10 @@ def test_anonymity(ip='127.0.0.1', port=9050):
 
 def scrape_identity(sex='m', country_code='us', nameset_code='us'):
     """Scrapes a randomly generated fake identity as given by 
-       fakenamegenerator.com.
+       fakenamegenerator.com. When successful, returns a list of
+       tuples, each tuple containing the element name (such as
+       "name", "birthdate" ...) and its value. Otherwise, return
+       False.
        
        Keyword arguements:
        country_code -- The country where the fake identy is from.
@@ -47,31 +57,23 @@ def scrape_identity(sex='m', country_code='us', nameset_code='us'):
     try:
         s = {'m' : 'male', 'f': 'female'}[sex]
     except KeyError as e:
-        print('argument sex is invalid. Either f for female or m for male')
-        return
+        print('argument sex is invalid. Either f for female or m for male.')
+        return False
         
     # Build our litte HTTP request. fakenamegenerator.com insits on playing
     # the sorehead, therefore we need to forge a normal looking browser request 
     # with acting like we have a browserlike UA.
     # Common UAs?
     # => http://techblog.willshouse.com/2012/01/03/most-common-user-agents/
-    build_url = 'http://fakenamegenerator.com/gen-{0}-{1}-{2}.php'.format(s, nameset_code, country_code)
+    build_url = FAKENAME_GENERATOR_URL.format(s, nameset_code, country_code)
     request = urllib.request.Request(build_url)
-    request.add_header('User-Agent',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31')
-    request.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8') # Maybe redundant.
+    request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1'
+						'; WOW64) AppleWebKit/537.31 (KHTML, like Gecko)'
+									'	Chrome/26.0.1410.64 Safari/537.31')
+    request.add_header('Accept', 'text/html,application/xhtml+xml,'
+									'application/xml;q=0.9,*/*;q=0.8')
 
-    identity = {
-        'full_name': '',
-        'address': '',
-        'gender': s,
-        'birthdate': '',
-        'phone_number': '',
-        'mother_maiden_name': '',
-        'blood_group': '',
-        'height': '',
-        'weight': '',
-    }
+    identity = []
     
     try:
         # Pass a file like object to the parser. This one liner
@@ -83,11 +85,12 @@ def scrape_identity(sex='m', country_code='us', nameset_code='us'):
         e = dom.find_class('address')
         for i in e[0].iterchildren():
             if i.tag == 'h3':
-                identity['full_name'] = i.text.strip()
+                identity.append(('full_name', i.text.strip()))
             if i.tag == 'div':
-                identity['address'] = i.text.strip()
+                identity.append(('address', i.text.strip()))
         # Get the phone number.
-        identity['phone_number'] = dom.find_class('tel')[0].getchildren()[0].text.strip()
+        identity.append(('phone_number', 
+                    dom.find_class('tel')[0].getchildren()[0].text.strip()))
         
         # Now it's getting increasingly complicated because we don't have any good 
         # needles to extract the elements we are interested in. We have to partly work around
@@ -102,23 +105,35 @@ def scrape_identity(sex='m', country_code='us', nameset_code='us'):
             if n.tag == 'li':
                 id_elements.append(n.text)
         
-        # Clean the list.
-        id_elements = [x.strip() for x in id_elements if x]
+        # Clean the list from None elements.
+        id_elements = [x.strip() for x in id_elements if x is not None]
         
-        # Now harvest the generated identity entities.
-        identity['mother_maiden_name'] = id_elements[2]
-        identity['birthdate'] = id_elements[3]
-        identity['blood_group'] = id_elements[13]
-        identity['weight'] = id_elements[14]
-        identity['height'] = id_elements[15]
+        # Now harvest the generated identity entities. Please keep in 
+        # mind that you can appened others pieces of information that
+        # remains in the id_elements list to your identity list. For example:
+        # username, password, visa, generated credit card number...
+        identity.append(('mother_maiden_name', id_elements[2]))
+        # Chop the parentheses, no need for them.
+        identity.append(('birthdate', id_elements[3][:id_elements[3].index(' (')]))
+        identity.append(('blood_group', id_elements[13]))
+        
+        # For the weight and height, use the International System of Units
+        # A weight looks like: "210.3 pounds (95.6 kilograms)", we transform it with
+        # some regular expression magic into "95.6 kilograms". Similar with the height.
+        r = re.compile('\((.+)\)')
+        identity.append(('weight', r.search(id_elements[14]).group(0)[1:-1]))
+        identity.append(('height', r.search(id_elements[15]).group(0)[1:-1]))
         
         return identity
+        
+    # Some connection error, either server or client side.
     except urllib.HTTPError as err:
-        if err.code != 404:
-            print('HTTP Error:', err.code)
+        print('HTTP Error: ', err.code)
+    # The URL is apparently invalid.
     except urllib.URLError as err:
-        print('HTTP Error:', err.code)
-    # Catching lxml specific errors.
+        print('URL Error: ', err.code)
+    # Catching lxml specific errors and other exceptions. Doesn't 
+    # catch SystemExit and KeyboardInterrupt exceptions though.
     except Exception as e:
         print('Some error occured while lxml tried to parse.', e[0].args)
  
@@ -130,15 +145,16 @@ def anon_identity():
        
     """
     
-    # Set up a socks socket. You might want to fire up your local TOR PROXY.
+    # Set up a socks socket. You might want to fire up your local TOR PROXY, before
+    # using this function.
     # Just download TOR here https://www.torproject.org/ and then start tor.
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,'127.0.0.1', 9050)
     socks.wrapmodule(urllib.request)
     
     id = scrape_identity()
     print('[+] Generated a random and anonymous identity:')
-    for e in id.keys():
-        print('\t{0:.<20}{1}'.format(e, id[e]))
+    for e in id:
+        print('\t{0:.<20}{1}'.format(e[0], e[1]))
         
 if __name__ == '__main__':
     anon_identity()
